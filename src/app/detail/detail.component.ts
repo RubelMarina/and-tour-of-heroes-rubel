4de80b0027fd46b9e4232e437c368dc56d08386b
MaRina <marina88rubel@gmail.com>
2021-09-15 10:22:52 +0300

    Create a feature component

---
+++ src/app/detail/detail.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+import {Hero} from "../hero";
+
+@Component({
+  selector: 'app-detail',
+  templateUrl: './detail.component.html',
+  styleUrls: ['./detail.component.css']
+})
+export class DetailComponent implements OnInit {
+  @Input() hero!: Hero;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
